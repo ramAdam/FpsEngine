af472d0ab2268932ff7b7c00b7fd7fb51654b37a
ramAdam <meerdara@hotmail.com>
2024-11-13 20:32:29 -0500

    Add initial headers and structures for geometry and entity management

--- qodot.h
+++ qodot.h
@@ -1,40 +1,58 @@
 #ifndef QODOT_H
 #define QODOT_H
-#include "core/dictionary.h"
-#include "core/reference.h"
-#include "core/ustring.h"
+
 #include "geo_generator.h"
 #include "map_parser.h"
 #include "surface_gatherer.h"
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
-class Qodot : public Reference {
-	GDCLASS(Qodot, Reference);
+class Qodot {
+private:
 	std::shared_ptr<LMMapData> map_data = std::make_shared<LMMapData>();
-	LMMapParser map_parser = LMMapParser(map_data);
-	LMGeoGenerator geo_generator = LMGeoGenerator(map_data);
-	LMSurfaceGatherer surface_gatherer = LMSurfaceGatherer(map_data);
+	LMMapParser map_parser;
+	LMGeoGenerator geo_generator;
+	LMSurfaceGatherer surface_gatherer;
 
 public:
-	void load_map(const String &map_file_str);
-	PoolStringArray get_texture_list();
-	void set_entity_definitions(Dictionary p_entity_defs);
-	void set_worldspawn_layers(Array p_worldspawn_layers);
-	void generate_geometry(Dictionary p_texture_dict);
-	Array get_entity_dicts();
-	Array get_worldspawn_layer_dicts();
-	void gather_texture_surfaces(const String p_texture_name, const String p_brush_filter_texture, const String p_face_filter_texture);
-	void gather_worldspawn_layer_surfaces(const String p_texture_name, const String p_brush_filter_texture, const String p_face_filter_texture);
-	void gather_entity_convex_collision_surfaces(int64_t p_entity_idx);
-	void gather_entity_concave_collision_surfaces(int64_t p_entity_idx);
-	void gather_worldspawn_layer_collision_surfaces(int64_t p_entity_idx);
-	Array fetch_surfaces(double p_inverse_scale_factor);
+	Qodot() :
+			map_parser(map_data),
+			geo_generator(map_data),
+			surface_gatherer(map_data) {}
 
-protected:
-	static void _bind_methods();
+	// Core functionality
+	void load_map(const std::string &map_file);
+	std::vector<std::string> get_texture_list();
+	void set_entity_definitions(std::unordered_map<std::string, std::string> entity_defs);
+	void set_worldspawn_layers(std::vector<std::string> worldspawn_layers);
+	void generate_geometry(std::unordered_map<std::string, std::string> texture_dict);
+
+	// Entity related
+	std::vector<std::unordered_map<std::string, std::string>> get_entity_dicts();
+	std::vector<std::unordered_map<std::string, std::string>> get_worldspawn_layer_dicts();
+
+	// Surface gathering
+	void gather_texture_surfaces(const std::string &texture_name,
+			const std::string &brush_filter_texture,
+			const std::string &face_filter_texture);
+
+	void gather_worldspawn_layer_surfaces(const std::string &texture_name,
+			const std::string &brush_filter_texture,
+			const std::string &face_filter_texture);
+
+	void gather_entity_convex_collision_surfaces(size_t entity_idx);
+	void gather_entity_concave_collision_surfaces(size_t entity_idx);
+	void gather_worldspawn_layer_collision_surfaces(size_t entity_idx);
+
+	std::vector<float> fetch_surfaces(double inverse_scale_factor);
 
 private:
-	void gather_texture_surfaces_internal(const String p_texture_name, const String p_brush_filter_texture, const String p_face_filter_texture, bool p_filter_layers);
-	void gather_convex_collision_surfaces(int64_t p_entity_idx, bool p_filter_layers);
-	void gather_concave_collision_surfaces(int64_t p_entity_idx, bool p_filter_layers);
+	void gather_texture_surfaces_internal(const std::string &texture_name,
+			const std::string &brush_filter_texture,
+			const std::string &face_filter_texture,
+			bool filter_layers);
 };
-#endif
+
+#endif // QODOT_H
